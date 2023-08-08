9614e89f10aaf4bc0a42d08771803f2184dec353
Zehan Ma <zehanma@berkeley.edu>
2023-08-08 15:40:52 -0700

    initial commit

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ setup.py
 PEP 621 switches most of Packaging to `pyproject.toml` -- yet keep a "dummy" setup.py for external code that has not
 yet upgraded.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+# setup()
+
+
+import os
+import sys
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+setup(name='r2d2', package_dir = {'':'.'}, packages=find_packages())
